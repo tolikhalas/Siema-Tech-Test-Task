03a592ecde90d2e50f3642f2cf2db2f1a6028e07
Anatolii Halas <tolikgalas@gmail.com>
2024-06-20 22:54:22 +0300

    BE-3-Users/ Implemented User entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column("text")
+  firstName: string;
+
+  @Column("text", { nullable: true })
+  lastName: string;
+
+  @Column("text", { unique: true })
+  email: string;
+
+  @Column("text")
+  password: string;
+}
