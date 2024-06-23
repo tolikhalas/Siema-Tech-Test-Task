18fdd73af14ecfd60992d339a696bda88aa75d75
Anatolii Halas <tolikgalas@gmail.com>
2024-06-23 23:22:02 +0300

    BE-5-Permissions/ Implemented Permission entity

--- src/permissions/entities/permission.entity.ts
+++ src/permissions/entities/permission.entity.ts
@@ -1,1 +1,10 @@
-export class Permission {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Permission {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+}
