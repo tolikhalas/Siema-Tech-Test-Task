066b9d1f27d89de0c0ac716bfd5947ba8c2e641c
Anatolii Halas <tolikgalas@gmail.com>
2024-06-23 23:20:39 +0300

    BE-5-Permissions/ Added permissions resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthModule } from "./auth/auth.module";
+import { PermissionsModule } from './permissions/permissions.module';
 import typeormConfig from "./config/typeorm.config";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig), UsersModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), UsersModule, AuthModule, PermissionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
