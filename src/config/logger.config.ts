dc9242f78d5f2631a71f7c2123eb00bef2277395
Anatolii Halas <tolikgalas@gmail.com>
2024-06-30 10:14:04 +0300

    MONO-2-Logging/ Added & configured winston logger

---
+++ src/config/logger.config.ts
@@ -0,0 +1,33 @@
+import {
+  WinstonModuleOptions,
+  utilities as nestWinstonModuleUtilities,
+} from "nest-winston";
+import * as winston from "winston";
+
+export const winstonConfig: WinstonModuleOptions = {
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        nestWinstonModuleUtilities.format.nestLike("MyApp", {
+          prettyPrint: true,
+        }),
+      ),
+    }),
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+    new winston.transports.File({
+      filename: "logs/combined.log",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+  ],
+};
