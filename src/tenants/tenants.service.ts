0e9e0762cd7ca5f453dddc7daa81a7153c1ed3d7
Anatolii Halas <tolikgalas@gmail.com>
2024-06-30 10:44:48 +0300

    MONO-2-Logging/ Added logging for TenantService

--- src/tenants/tenants.service.ts
+++ src/tenants/tenants.service.ts
@@ -4,16 +4,25 @@ import { UpdateTenantDto } from "./dto/update-tenant.dto";
 import { Repository } from "typeorm";
 import { Tenant } from "./entities/tenant.entity";
 import { plainToClass } from "class-transformer";
+import { WINSTON_MODULE_PROVIDER } from "nest-winston";
+import { Logger } from "winston";
 
 @Injectable()
 export class TenantsService {
   constructor(
     @Inject("TENANT_REPOSITORY")
     private readonly tenantsRepository: Repository<Tenant>,
+    @Inject(WINSTON_MODULE_PROVIDER)
+    private readonly logger: Logger,
   ) {}
 
   async create(createTenantDto: CreateTenantDto) {
     const tenant = await this.tenantsRepository.save(createTenantDto);
+
+    this.logger.info(`Tenant created with id[${tenant.id}]`, {
+      ...tenant,
+    });
+
     return plainToClass(Tenant, tenant, { excludeExtraneousValues: true });
   }
 
@@ -32,11 +41,17 @@ export class TenantsService {
   async update(id: number, updateTenantDto: UpdateTenantDto) {
     const tenant = await this.findOne(id);
     Object.assign(tenant, updateTenantDto);
+
+    this.logger.log(`Updating tenant with id: ${tenant.id}`, tenant);
+
     return await this.tenantsRepository.save(tenant);
   }
 
   async remove(id: number) {
     const tenant = await this.findOne(id);
+
+    this.logger.log(`Removing tenant with id: ${tenant.id}`, tenant);
+
     return await this.tenantsRepository.remove(tenant);
   }
 }
